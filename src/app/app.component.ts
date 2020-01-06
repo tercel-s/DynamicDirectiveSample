e524f2f495b8a2e15a21a7bfcef12cdb94484bb4
tercel_s <0x12f345c@gmail.com>
2020-01-06 19:35:46 +0900

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, AfterViewInit, TemplateRef, NgZone } from '@angular/core';
+import { Portal, TemplatePortal } from '@angular/cdk/portal';
+import * as uuid from 'uuid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DynamicDirectiveSample';
+export class AppComponent implements AfterViewInit {
+  readonly id = `c${uuid.v4()}`;
+
+  @ViewChild('grabbableContent', { static: false }) grabbableContent: TemplateRef<any>;
+  @ViewChild('notGrabbableContent', { static: false }) notGrabbableContent: TemplateRef<any>;
+
+  selectedPortal: Portal<any>;
+  grabbableContentPortal: TemplatePortal<any>;
+  notGrabbableContentPortal: TemplatePortal<any>;
+
+  grabbable = true;
+
+  constructor(private viewContainerRef: ViewContainerRef, private ngZone: NgZone) { }
+
+  ngAfterViewInit() {
+    new Promise<void>(resolve => {
+      this.grabbableContentPortal = new TemplatePortal(this.grabbableContent, this.viewContainerRef);
+      this.notGrabbableContentPortal = new TemplatePortal(this.notGrabbableContent, this.viewContainerRef);
+      resolve();
+    }).then(() => this.selectedPortal = this.grabbableContentPortal);
+  }
+
+  setGrabbable(enabled: boolean) {
+    this.grabbable = enabled;
+    this.selectedPortal = enabled ? this.grabbableContentPortal : this.notGrabbableContentPortal;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PortalModule } from '@angular/cdk/portal';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PortalModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
